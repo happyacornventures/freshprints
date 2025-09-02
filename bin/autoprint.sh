dfb87006fb422f50baaf851025b7de0c1ce5ae4c
Randy Brown <thatrandybrown@gmail.com>
2025-09-01 21:39:56 -0500

    allow optionally designating the target printer

--- bin/autoprint.sh
+++ bin/autoprint.sh
@@ -2,10 +2,16 @@
 # Usage: ./autoprint.sh <file-to-print> [printer-name]
 
 FILE="$1"
+PRINTER="$2"
 
 if [ -z "$FILE" ]; then
   echo "Usage: $0 <file-to-print> [printer-name]"
   exit 1
 fi
 
-lp "$FILE"
+if [ -z "$PRINTER" ]; then
+  # Use default printer
+  lp "$FILE"
+else
+  lp -d "$PRINTER" "$FILE"
+fi
